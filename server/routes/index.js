f43d411234bfb418a9c07a30081ffb11b715023f
Saharsh Shrivastava <76205567+30saharsh@users.noreply.github.com>
2023-08-09 23:52:06 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,7 +16,7 @@ router.use(cors({
 mongoose.connect('mongodb+srv://saharshshrivastava302002:ODqlSR8qYpN1hxQH@cluster0.lcjavdi.mongodb.net/Blog_App_Database?retryWrites=true&w=majority')
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json("Hello");
 });
 // This Route add the input data of blog into the database 
 router.post('/adddata', function(req, res, next) {
